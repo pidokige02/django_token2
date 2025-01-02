9cd9cf6a37fe659982b996bd6a835946ebe6ddcb
pidokige02 <pidokige0204@gmail.com>
2025-01-02 13:25:02 +0900

    refinne urll for Home directory

--- authentication_vuetify/src/components/AppNav.vue
+++ authentication_vuetify/src/components/AppNav.vue
@@ -1,31 +1,11 @@
 <template>
-  <v-app>
-    <v-container>
-      <div id="nav">
-        <!-- Home 링크 -->
-        <v-btn to="/" text>Home</v-btn>
-
-        <!-- Dashboard 링크 (로그인 상태일 때) -->
-        <v-btn v-if="loggedIn" to="/dashboard" text>Dashboard</v-btn>
-
-        <!-- 로그인 버튼 (로그인 안 된 상태일 때) -->
-        <v-btn v-if="!loggedIn" to="/login" class="button" color="primary">
-          Login
-        </v-btn>
-
-        <!-- 로그아웃 버튼 (로그인 상태일 때) -->
-        <v-btn
-          v-else
-          type="button"
-          class="logoutButton"
-          @click="logout"
-          color="red"
-        >
-          Logout
-        </v-btn>
-      </div>
-    </v-container>
-  </v-app>
+  <v-app-bar app color="teal" dark>
+    <v-btn to="/" text>Home</v-btn>
+    <v-spacer></v-spacer>
+    <v-btn v-if="loggedIn" to="/dashboard" text>Dashboard</v-btn>
+    <v-btn v-if="!loggedIn" to="/login" text>Login</v-btn>
+    <v-btn v-else text @click="logout">Logout</v-btn>
+  </v-app-bar>
 </template>
 
 <script>
@@ -49,52 +29,6 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-#nav {
-  display: flex;
-  align-items: center;
-  min-height: 50px;
-  padding: 0.2em 1em;
-  background: linear-gradient(to right, #16c0b0, #84cf6a);
-}
-
-.nav-welcome {
-  margin-left: auto;
-  margin-right: 1rem;
-  color: white;
-}
-
-a {
-  font-weight: bold;
-  color: #2c3e50;
-  margin: auto 0.8em auto 0.4em;
-  text-decoration: none;
-  border-top: 2px solid transparent;
-  border-bottom: 2px solid transparent;
-}
-
-.router-link-exact-active {
-  color: white;
-  border-bottom: 2px solid #fff;
-}
-
-button,
-.button {
-  margin-left: auto;
-  background: white;
-  text-decoration: none;
-  color: #2c3e50;
-
-  &.router-link-active {
-    color: #2c3e50;
-  }
-}
-
-.logoutButton {
-  cursor: pointer;
-}
-
-.nav-welcome + button {
-  margin-left: 0;
-}
+<style scoped>
+/* Vuetify에서 app-bar와 버튼을 사용하므로 추가적인 스타일링은 필요하지 않습니다. */
 </style>
